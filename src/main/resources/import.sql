38379bbb3ac50a3af1f79d0f68b03f2ebfe7c4ec
carlucios <carlucios@gmail.com>
2023-07-23 17:33:55 -0300

    Ajustando o seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,10 @@
-INSERT INTO `myTable` (`name`,`cpf`,`income`,`birth_date`,`children`)
-VALUES
-    ("Lee Gross","734.208.405-76","$82.60","Sep 25, 2023",2),
-    ("Calista Woods","403.384.339-62","$29.47","Oct 8, 2023",0),
-    ("Ryder Ramirez","441.882.646-61","$8.55","May 2, 2024",1),
-    ("Griffith Perez","871.669.117-98","$74.80","Jun 21, 2023",2),
-    ("Jared Ruiz","073.851.356-52","$81.98","Jan 2, 2024",3),
-    ("Luke Horton","923.212.071-28","$77.22","Jul 11, 2024",2),
-    ("Kristen Gallegos","453.586.184-56","$66.77","Feb 18, 2023",2),
-    ("Raphael Meyers","761.458.943-21","$97.39","Nov 4, 2022",3),
-    ("Geoffrey Velasquez","371.614.608-27","$69.86","Sep 1, 2023",4),
-    ("Indigo Wilkinson","718.785.514-50","$98.14","Sep 19, 2023",0);
+INSERT INTO tb_cliente (name,cpf,income,birth_date,children) VALUES ('Lee Gross','734.208.405-76',8260,'2003-09-25',2);
+INSERT INTO tb_cliente (name,cpf,income,birth_date,children) VALUES ('Calista Woods','403.384.339-62',2947,'1993-10-08',0);
+INSERT INTO tb_cliente (name,cpf,income,birth_date,children) VALUES ('Ryder Ramirez','441.882.646-61',855,'1984-05-02',1);
+INSERT INTO tb_cliente (name,cpf,income,birth_date,children) VALUES ('Griffith Perez','871.669.117-98',7480,'1953-06-21',2);
+INSERT INTO tb_cliente (name,cpf,income,birth_date,children) VALUES ('Jared Ruiz','073.851.356-52',8198,'1944-02-02',3);
+INSERT INTO tb_cliente (name,cpf,income,birth_date,children) VALUES ('Luke Horton','923.212.071-28',7722,'1974-07-11',2);
+INSERT INTO tb_cliente (name,cpf,income,birth_date,children) VALUES ('Kristen Gallegos','453.586.184-56',6677,'1999-02-18',2);
+INSERT INTO tb_cliente (name,cpf,income,birth_date,children) VALUES ('Raphael Meyers','761.458.943-21',9739,'2000-10-04',3);
+INSERT INTO tb_cliente (name,cpf,income,birth_date,children) VALUES ('Geoffrey Velasquez','371.614.608-27',6986,'1993-01-01',4);
+INSERT INTO tb_cliente (name,cpf,income,birth_date,children) VALUES ('Indigo Wilkinson','718.785.514-50',9814,'1963-09-19',0);
